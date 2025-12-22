a3cf3156d3437fdc71b4b57079d676b4a2c89cfc
MiniBaker007 <jean.thomas.letourneau@gmail.com>
2025-12-22 11:04:21 -0500

    Bring back no integer conversion

--- lib/miniaudio_stbvorbis.c
+++ lib/miniaudio_stbvorbis.c
@@ -1,4 +1,5 @@
 #define STB_VORBIS_HEADER_ONLY
+#define STB_VORBIS_NO_INTEGER_CONVERSION
 #include "extras/stb_vorbis.c"
 #include "extras/miniaudio_split/miniaudio.c"
 #undef STB_VORBIS_HEADER_ONLY
